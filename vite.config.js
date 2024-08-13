e5145b6cb0d25aeacaa4d68527d50fe541a590dc
gojogourav <gojogourav@gmail.com>
2024-08-13 10:58:54 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Password_Generator ",
+  base: "https://gojogourav.github.io/Password_Generator ",
   plugins: [react()],
 })
